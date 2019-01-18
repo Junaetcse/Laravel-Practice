478f422dc653d5370b4cdfe8d7f5c162f48b81ea
JunaeT420 <dkcks786@gmail.com>
2019-01-18 23:28:27 +0600

    Practices query builder

--- laravel-practices/app/Http/Controllers/HomeController.php
+++ laravel-practices/app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Mail\SendMailable;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
 
 class HomeController extends Controller
@@ -15,5 +16,17 @@ class HomeController extends Controller
 
         Mail::to('junaetcse@gmail.com')->send(new SendMailable());
         return 'Email was sent';
+    }
+
+
+
+
+    public function sql(){
+
+    	$users = DB::table('users')->get();
+    	// $user = DB::table('users')->where('name','Friedrich Bergnaum')->first();
+    	// return $user->name;
+    	$user = DB::table('users')->where('name','Friedrich Bergnaum')->value('email');
+    	return $user;
     }
 }
