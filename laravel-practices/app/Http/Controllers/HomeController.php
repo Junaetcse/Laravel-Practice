e0e8842b033c323f4e841b9241e85e3665ae9456
junaet_hossain <junaetcse@gmal.com>
2019-01-20 00:59:15 +0600

    Completed sql query from doc

--- laravel-practices/app/Http/Controllers/HomeController.php
+++ laravel-practices/app/Http/Controllers/HomeController.php
@@ -24,7 +24,10 @@ class HomeController extends Controller
     public function sql(){
 
        // $this->basicSql();
-        $this->sqlPart2();
+       //$this->sqlPart2();
+       //$this->sqlJoin();
+       //$this->sqlWhereCluse();
+       $this->qslOrder();
     }
 
 
@@ -47,10 +50,40 @@ class HomeController extends Controller
         $dis = DB::table('users')->distinct()->get();
         $query = DB::table('users')->select('name');
         $dd = $query->addSelect('email')->get();
-        
-       // dump($query);
         return $dis;
+    }
 
+
+    public function sqlJoin(){
+        $value = DB::table('users')->crossJoin('contacts')->get();
+        $value2 = DB::table('users')->join('contacts',function ($join){
+            $join->on('users.id','=', 'contacts.user_id');
+        })->get();
+    }
+
+
+    public function sqlWhereCluse(){
+        // Difference between two query
+        //select * from `activities` where `ranking` = ? and `status` = ?
+        //select * from `activities` where (`ranking` = ? and `status` = ?)
+        $info = DB::table('activities')->where([['ranking', '=', 3],['status', '=', 'Ut deleniti ut optio neque.']])->get();
+        $info = DB::table('activities')->where('ranking', '=', 3)->where('status', '=', 'Ut deleniti ut optio neque.')->get();
+        $info = DB::table('activities')->where('ranking', '=', 3)->orWhere('status', '=', 'Ut deleniti ut optio neque.')->get();
+        //between where clauses
+        $info = DB::table('activities')->whereBetween('ranking', [1,3])->get(); // select * from `activities` where `ranking` between ? and ?
+        //compare column to column if same data
+        DB::table('activities')->whereColumn('contact_id','ranking')->get(); //select * from `activities` where `contact_id` = `ranking`
+        DB::table('users')->whereExists(function ($query){$query->select(DB::raw(info))->from('contacts')->whereRaw('contacts.user_id = users.id');})->get();
+        return $info;
+    }
+
+
+    public function qslOrder(){
+        DB::table('users')->latest()->first(); //select * from `users` order by `created_at` desc
+        DB::table('users')->inRandomOrder()->first();
+        DB::table('contacts')->groupBy('user_id')->having('user_id','>',20)->get();
+        DB::table('users')->skip(10)->take(5)->get(); //select * from `users` limit 5 offset 10
+        
     }
 
 }
